ef1b0aeaaca29d841210f33f6d48bf8d8ca2cf40
LM\N0269887 <glen.miskimmons@liberty-it.co.uk>
2018-11-01 06:38:01 -0400

    Pluralsight Course
    Java Fundamentals: Collections

---
+++ src/com/pluralsight/collections/_5_queues/c_stacks/Calculator.java
@@ -0,0 +1,52 @@
+package com.pluralsight.collections._5_queues.c_stacks;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Calculator{
+    /*
+    1
+    -
+    3
+    +
+    2
+    +
+    4
+    */
+
+    public int evaluate(final String input){
+        final Deque<String> stack = new ArrayDeque<>();
+        final String[] tokens = input.split(" ");
+
+        for(String command : tokens ) {
+            stack.add(command);
+        }
+
+        while(stack.size() > 1) {
+            int left = Integer.parseInt(stack.pop());
+            String operator = stack.pop();
+            int right = Integer.parseInt(stack.pop());
+
+            int result = 0;
+
+            switch(operator) {
+                case "+":
+                    result = left + right;
+                    break;
+                case "-":
+                    result = left - right;
+                    break;
+                case "*":
+                    result = left * right;
+                    break;
+                case "/":
+                    result = left / right;
+                    break;
+            }
+
+            stack.push(String.valueOf(result));
+        }
+
+        return Integer.parseInt(stack.pop());
+    }
+}
