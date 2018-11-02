672130dcf39a2f0072701080472297433f557c41
LM\N0269887 <glen.miskimmons@liberty-it.co.uk>
2018-11-02 06:49:49 -0400

    Pluralsight Course
    Java Fundamentals: Collections

---
+++ src/com/pluralsight/collections/_6_maps/ProductLookupTable.java
@@ -0,0 +1,10 @@
+package com.pluralsight.collections._6_maps;
+
+public interface ProductLookupTable
+{
+    Product lookupById(int id);
+
+    void addProduct(Product productToAdd);
+
+    void clear();
+}
